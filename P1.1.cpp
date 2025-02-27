db71d313ed4ea566a07e2c2dbd9a6e540bac2272
Your Name <you@example.com>
2025-02-27 09:25:17 -0600

    basic setted

---
+++ P1.1.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define MAX_LENGTH 100
+
+void mainAlgorythm(int*, int);
+
+void showArray(int*, int);
+void generateRandomArray(int*, int);
+void generateBestCaseArray(int*, int);
+void generateWorstCaseArray(int*, int);
+
+int main(){
+    srand(time(NULL));
+    int array [MAX_LENGTH];
+
+    for(int i = 0; i < 10; i++){
+        generateRandomArray(array, 10);
+        showArray(array, 10);
+        mainAlgorythm(array, 10);
+    }
+    return 1;
+}
+
+void mainAlgorythm(int array [], int n){
+    int i, j;
+
+    for(i = 0; i < n/2; i++){
+        for(j = 0; j < n; j ++){
+            if(array[i] == array[j]){
+                printf("Numero encontrado: %d.\n Posiciones: [%d],[%d]", array[i], i, j);
+                return;
+            }
+        }
+    }
+
+    printf("No se ha encontrado ningún dato.\n");
+}
+
+void showArray(int array[], int n){
+    int i;
+    for(i = 0; i < n; i++)
+        printf("%d, ", array[i]);
+    printf("\n");
+}
+
+void generateRandomArray(int array[], int n){
+    int i;
+    for(i = 0; i < n; i++){
+        array[i] = rand() % (3 * n);
+    }
+}
